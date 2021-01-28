b8e97029422cdc8574f951314c75c061adf08a7e
Shamrai <shamrai.dev@gmail.com>
2021-01-28 15:00:37 +0200

    UI + logick

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -12,7 +12,30 @@ class ViewController: UIViewController {
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
-
-
+    @IBOutlet weak var labelTitle: UILabel!
+    @IBOutlet weak var labelHint: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.becomeFirstResponder() // To get shake gesture
+        
+        labelTitle.text = "Запитай будь-що..."
+        labelHint.text = "Потруси мене"
+    }
+    
+    // We are willing to become first responder to get shake motion
+    override var canBecomeFirstResponder: Bool {
+        get {
+            return true
+        }
+    }
+    
+    // Enable detection of shake motion
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            imageView.image = ballArray.randomElement()
+        }
+    }
 }
 
